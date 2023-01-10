3ec2e52ef107951e18443ac0372a63ef2b03f198
ducng99 <tom@ducng.dev>
2023-01-10 19:49:58 +1300

    feat: Sidebar is responsive

--- src/common/Sidebar.tsx
+++ src/common/Sidebar.tsx
@@ -1,19 +1,40 @@
 import { Link } from "react-router-dom";
 
+import("./Sidebar.css");
+import("bootstrap/js/dist/collapse");
+
 export default function Sidebar() {
 	return (
-		<div className="col-1 border-end" style={{ height: "100vh" }}>
-			<h1>Tools</h1>
-			<small>By human or machine</small>
+		<div className="navbar navbar-dark navbar-expand-lg col-12 col-md-3 col-xl-2 px-sm-2 px-0 bg-dark" id="sidebar">
+			<div className="d-flex flex-column align-items-start px-3 pt-2 w-100 text-white">
+				<div className="d-flex">
+					<button className="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<h1>Tools</h1>
+				</div>
 
-			<ul className="nav flex-column">
-				<li className="nav-item">
-					<Link className="nav-link" to={'csv_swap'}>CSV Swap</Link>
-				</li>
-				<li className="nav-item">
-					<Link className="nav-link" to={'csv_display_table'}>CSV Display Table</Link>
-				</li>
-			</ul>
+				<ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start collapse navbar-collapse w-100" id="menu">
+					<li className="nav-item">
+						<Link to="csv_swap" className="nav-link align-middle px-0">
+							<span className="ms-1">CSV Swap</span>
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="csv_display_table" className="nav-link align-middle px-0">
+							<span className="ms-1">CSV Display table</span>
+						</Link>
+					</li>
+					<div className="d-md-none mb-3 w-100">
+					<hr />
+					<small>© 2022 - {new Date().getFullYear()} Thomas Nguyen</small>
+				</div>
+				</ul>
+				<div className="d-none d-md-block mb-3 w-100">
+					<hr />
+					<small>© 2022 - {new Date().getFullYear()} Thomas Nguyen</small>
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,29 +4,32 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App'
 
 import('bootstrap/dist/css/bootstrap.min.css');
+import('bootstrap-icons/font/bootstrap-icons.css');
 
 const CSV_Swap = lazy(() => import('./tools/csv_swap'));
 const CSV_Display_Table = lazy(() => import('./tools/csv_display_table'));
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				path: "/csv_swap",
-				element: <CSV_Swap />
-			},
-			{
-				path: "/csv_display_table",
-				element: <CSV_Display_Table />
-			}
-		]
-	}
-],
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <App />,
+			children: [
+				{
+					path: "csv_swap",
+					element: <CSV_Swap />
+				},
+				{
+					path: "csv_display_table",
+					element: <CSV_Display_Table />
+				}
+			]
+		}
+	],
 	{
 		basename: '/tools'
-	});
+	}
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
