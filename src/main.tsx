3b6f17d37908ee77f115433a0602b0f80bc7adbb
ducng99 <tom@ducng.dev>
2023-01-10 19:52:05 +1300

    fix: Remove bootstrap icons css being imported

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App'
 
 import('bootstrap/dist/css/bootstrap.min.css');
-import('bootstrap-icons/font/bootstrap-icons.css');
 
 const CSV_Swap = lazy(() => import('./tools/csv_swap'));
 const CSV_Display_Table = lazy(() => import('./tools/csv_display_table'));
