bfcf95f588629c9fe4200bfd8cdad963f883d028
ducng99 <tom@ducng.dev>
2023-05-14 02:19:10 +1200

    Hardcode current year

--- src/common/SidebarFooter.tsx
+++ src/common/SidebarFooter.tsx
@@ -2,7 +2,7 @@ function SidebarFooter() {
     return (
         <>
             <hr />
-            <small>&copy; 2022 - {new Date().getFullYear()} Thomas Nguyen</small>
+            <small>&copy; 2022 - 2023 Thomas Nguyen</small>
             <br />
             <a href="https://github.com/ducng99/tools" className="text-decoration-none text-white" target="_blank" rel="noreferrer"><i className="bi bi-github" /></a>
             <small>&nbsp;Code licensed <a className="text-decoration-none text-white" href="https://github.com/ducng99/tools/blob/main/LICENSE" target="_blank" rel="noreferrer">MIT</a></small>
