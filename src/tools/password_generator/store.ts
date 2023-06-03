ebb3e661ce57996e583c251c1545e595bd73c82a
ducng99 <tom@ducng.dev>
2023-06-03 13:52:51 +1200

    chore(password_generator): Extended default password symbols

--- src/tools/password_generator/store.ts
+++ src/tools/password_generator/store.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-export const DEFAULT_SYMBOLS = '!@#$%^&*()_-+=';
+export const DEFAULT_SYMBOLS = '!@#$%^&*()_-+=[]{};:\'",<.>/?';
 
 export interface PasswordOptions {
     length: number;
