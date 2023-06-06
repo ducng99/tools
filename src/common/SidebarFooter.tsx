d5ba5836208b3d9b70300a05b96d8e0bd1d96e13
ducng99 <tom@ducng.dev>
2023-06-06 13:14:10 +1200

    fix: Fixed sidebar entry incorrect active colour

--- src/common/SidebarFooter.tsx
+++ src/common/SidebarFooter.tsx
@@ -2,10 +2,12 @@ function SidebarFooter() {
     return (
         <>
             <hr />
-            <small>&copy; 2022 - 2023 Thomas Nguyen</small>
-            <br />
-            <a href="https://github.com/ducng99/tools" className="text-decoration-none" target="_blank" rel="noreferrer"><i className="bi bi-github" /></a>
-            <small>&nbsp;Code licensed <a className="text-decoration-none" href="https://github.com/ducng99/tools/blob/main/LICENSE" target="_blank" rel="noreferrer">MIT</a></small>
+            <div id="footer">
+                <small>&copy; 2022 - 2023 Thomas Nguyen</small>
+                <br />
+                <a href="https://github.com/ducng99/tools" className="text-decoration-none" target="_blank" rel="noreferrer"><i className="bi bi-github" /></a>
+                <small>&nbsp;Code licensed <a className="text-decoration-none" href="https://github.com/ducng99/tools/blob/main/LICENSE" target="_blank" rel="noreferrer">MIT</a></small>
+            </div>
         </>
     );
 }
