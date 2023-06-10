f9cb0739ac98d88e9c58cfd3acdf781ec19e2b43
ducng99 <tom@ducng.dev>
2023-06-11 02:34:22 +1200

    feat(barcode_utils): Added camera support for barcode reader

--- src/Utils.ts
+++ src/Utils.ts
@@ -5,3 +5,17 @@ export function escapeRegExp(string: string) {
 export function toFirstUpperCase(string: string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function clampWidthHeight(width: number, height: number, maxWidth: number, maxHeight: number) {
+    if (width > maxWidth) {
+        height = height * maxWidth / width;
+        width = maxWidth;
+    }
+
+    if (height > maxHeight) {
+        width = width * maxHeight / height;
+        height = maxHeight;
+    }
+
+    return [width, height];
+}
