2505aa477e3e041b8f7ee2beb56cdd13943d97e0
ducng99 <tom@ducng.dev>
2023-06-13 00:32:03 +1200

    Fixed cypress barcode reader tests

---
+++ cypress/e2e/tools/barcode_reader.cy.ts
@@ -0,0 +1,31 @@
+const barFiles = [
+    ['cypress/fixtures/tools/barcode_reader/url.png', 'https://static.ducng.dev/tools'],
+    ['cypress/fixtures/tools/barcode_reader/url.svg', 'https://static.ducng.dev/tools'],
+    ['cypress/fixtures/tools/barcode_reader/multi-lines.png', 'test\nmulti\nlines'],
+    ['cypress/fixtures/tools/barcode_reader/multi-lines.svg', 'test\nmulti\nlines'],
+    ['cypress/fixtures/tools/barcode_reader/special-chars.png', '!@#$%^&*()_-+=[]{};:\'",<.>/?'],
+    ['cypress/fixtures/tools/barcode_reader/special-chars.svg', '!@#$%^&*()_-+=[]{};:\'",<.>/?'],
+    ['cypress/fixtures/tools/barcode_reader/colours.png', 'test colours'],
+    ['cypress/fixtures/tools/barcode_reader/colours.svg', 'test colours'],
+    ['cypress/fixtures/tools/barcode_reader/custom-shapes-highres.png', 'this is a very high resolution image'],
+    ['cypress/fixtures/tools/barcode_reader/custom-shapes-highres.svg', 'this is a very high resolution image'],
+    ['cypress/fixtures/tools/barcode_reader/madness.png', 'no clue\nwhat this is\nmate\n1234657890-=_+[]:"}{,/.><?\nno clue\nwhat this is\nmate\n1234657890-=_+[]:"}{,/.><?\nno clue\nwhat this is\nmate\n1234657890-=_+[]:"}{,/.><?\nno clue\nwhat this is\nmate\n1234657890-=_+[]:"}{,/.><?\nno clue\nwhat this is\nmate\n1234657890-=_+[]:"}{,/.><?']
+];
+
+describe('Barcode reader', () => {
+    beforeEach(() => {
+        cy.visit('barcode_reader');
+        cy.get('#barcode-image-file-upload', { timeout: 15000 });
+        cy.get('#trigger-button', { timeout: 15000 });
+        cy.get('#output-textbox', { timeout: 15000 });
+    });
+
+    barFiles.forEach((file, index) => {
+        it('File upload test ' + index.toString(), () => {
+            cy.get('#barcode-image-file-upload').selectFile(file[0]);
+            cy.get('#output-textbox').should('have.value', file[1]);
+            cy.get('#trigger-button').click();
+            cy.get('#output-textbox').should('have.value', file[1]);
+        });
+    });
+});
