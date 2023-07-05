413b589914844e587396e07464f978cc306c8e55
ducng99 <tom@ducng.dev>
2023-07-05 22:03:13 +1200

    chore(dev-deps): Bump eslint-config-standard-with-typescript to 36.0.0
    Adapted eslint config to its breaking change

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:5173/#/",
-    video: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    e2e: {
+        baseUrl: 'http://localhost:5173/#/',
+        video: false,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        }
+    }
 });
