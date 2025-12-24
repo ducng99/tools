1b5a4ac0f095b872e20f719f27058124d5298faf
ducng99 <tom@tomng.dev>
2025-12-24 22:42:10 +1300

    updated missing files

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,41 +1,58 @@
 import {
     HeadContent,
-    Outlet,
     Scripts,
-    createRootRouteWithContext,
+    createRootRoute,
 } from "@tanstack/solid-router";
 import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
 
 import { HydrationScript } from "solid-js/web";
-import { Suspense } from "solid-js";
+import NotFoundComponent from "../components/common/NotFound";
+import ErrorComponent from "../components/common/ErrorComponent";
+import type { JSX } from "solid-js";
 
-import styleCss from "../styles.css?url";
-
-export const Route = createRootRouteWithContext()({
+export const Route = createRootRoute({
     head: () => ({
+        meta: [
+            {
+                title: "Tools",
+            },
+            {
+                charset: "utf-8",
+            },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+        ],
         links: [
-            { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" },
-            { rel: "stylesheet", href: styleCss },
+            { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3/dist/css/bootstrap.min.css" },
         ],
         scripts: [
-            { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" },
+            { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3/dist/js/bootstrap.bundle.min.js", type: "text/javascript" },
         ],
     }),
     shellComponent: RootComponent,
+    notFoundComponent: NotFoundComponent,
+    errorComponent: ErrorComponent,
 });
 
-function RootComponent() {
+function RootComponent({ children }: { children: JSX.Element }) {
     return (
-        <html>
+        <html lang="en">
             <head>
                 <HydrationScript />
             </head>
             <body>
                 <HeadContent />
-                <Suspense>
-                    <Outlet />
-                    <TanStackRouterDevtools />
-                </Suspense>
+                <div class="container-fluid">
+                    <div class="row flex-column flex-md-row min-vh-100">
+                        {/* <Sidebar /> */}
+                        <div class="col">
+                            {children}
+                        </div>
+                    </div>
+                </div>
+                <TanStackRouterDevtools />
                 <Scripts />
             </body>
         </html>
