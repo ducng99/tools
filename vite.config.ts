3b0195aae012066a241c58aec675a0bbb94e4830
ducng99 <tom@tomng.dev>
2026-05-04 11:29:13 +1200

    chore(deps): bump all packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
 import solidPlugin from "vite-plugin-solid";
@@ -11,9 +10,6 @@ export default defineConfig({
         devtools(),
         cloudflare({ viteEnvironment: { name: "ssr" } }),
         // this is the plugin that enables path aliases
-        viteTsConfigPaths({
-            projects: ["./tsconfig.json"],
-        }),
         tanstackStart(),
         solidPlugin({ ssr: true }),
     ],
